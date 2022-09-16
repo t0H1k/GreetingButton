2b5ae95533a9792c13505156fb9c0f04f4aa43ac
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-09-16 13:35:47 +0300

    Initial Commit

---
+++ GreetingButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreetingButton
+//
+//  Created by Anton Boev on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
